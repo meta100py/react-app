69371c30a320fc4740a339e262735742594c9733
meta100py <meta100py@gmail.com>
2024-11-10 12:55:02 -0300

    React app
    Added axios and openweather api

---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(`The weather in ${response.data.name} is ${response.data.main.temp}`);
+  }
+  let apiKey = "a710bd8bd76400c9658ef649d9e81728";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  return <h2>Hello from Weather</h2>;
+}
