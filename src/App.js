235b2a3068922f4b89230ddb1399ed6a55e044cd
meta100py (Valen's 2nd account) <meta100py@gmail.com>
2025-01-31 13:03:42 -0300

    changed the city

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World!!!</h1>
-        <Weather city="Tokyo" />
+        <Weather city="London" />
       </header>
     </div>
   );
